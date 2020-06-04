e005744f9ba62382d0d4352236fc6ec8901ab41a
Lucas Nascimento <camposs.nascimento@gmail.com>
2020-06-04 18:03:36 -0300

    Add new routes

--- petcontrolapi/routes/api.php
+++ petcontrolapi/routes/api.php
@@ -3,4 +3,7 @@
 use Illuminate\Http\Request;
 
 Route::get("clientes", "ClientesController@getCliente");
+Route::get("clientes/{clientes}", "ClientesController@getClienteById");
 Route::post("clientes", "ClientesController@postCliente");
+Route::patch("clientes/{clientes}", "ClientesController@updateCliente");
+Route::delete("clientes/{clientes}", "ClientesController@deleteCliente");
