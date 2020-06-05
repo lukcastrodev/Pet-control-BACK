75b4d4887ed5154c7ead83eddabf6f3f239f4cb1
Lucas Nascimento <camposs.nascimento@gmail.com>
2020-06-05 04:18:33 -0300

    Pet and user routes added

--- petcontrolapi/routes/api.php
+++ petcontrolapi/routes/api.php
@@ -7,3 +7,15 @@ Route::get("clientes/{clientes}", "ClientesController@getClienteById");
 Route::post("clientes", "ClientesController@postCliente");
 Route::patch("clientes/{clientes}", "ClientesController@updateCliente");
 Route::delete("clientes/{clientes}", "ClientesController@deleteCliente");
+
+Route::get("usuarios", "UsuariosController@getUsuario");
+Route::get("usuarios/{usuarios}", "UsuariosController@getUsuarioById");
+Route::post("usuarios", "UsuariosController@postUsuario");
+Route::patch("usuarios/{usuarios}", "UsuariosController@updateUsuario");
+Route::delete("usuarios/{usuarios}", "UsuariosController@deleteUsuario");
+
+Route::get("pets", "PetsController@getPet");
+Route::get("pets/{pets}", "PetsController@getPetById");
+Route::post("pets", "PetsController@postPet");
+Route::patch("pets/{pets}", "PetsController@updatePet");
+Route::delete("pets/{pets}", "PetsController@deletePet");
