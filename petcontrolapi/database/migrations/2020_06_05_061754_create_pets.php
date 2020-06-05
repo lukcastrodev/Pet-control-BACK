9fd8d25e3cdb908f819fb86a7246cd8fc1ca2317
Lucas Nascimento <camposs.nascimento@gmail.com>
2020-06-05 03:19:27 -0300

    Pets migration created

---
+++ petcontrolapi/database/migrations/2020_06_05_061754_create_pets.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePets extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome');
+            $table->string('tipo');
+            $table->string('raca');
+            $table->string('peso');
+            $table->string('cor');
+            $table->text('descricao');
+            $table->integer('cliente_id')->unsigned();
+            $table->foreign('cliente_id')->references('id')->on('clientes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pets');
+    }
+}
