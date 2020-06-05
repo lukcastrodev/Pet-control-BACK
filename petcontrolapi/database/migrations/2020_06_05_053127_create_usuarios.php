9c7c5a7276dff24d1d12688ae86f6ffcc6f63b13
Lucas Nascimento <camposs.nascimento@gmail.com>
2020-06-05 03:17:00 -0300

    User migration created

---
+++ petcontrolapi/database/migrations/2020_06_05_053127_create_usuarios.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsuarios extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome');
+            $table->string('cpf');
+            $table->string('email')->unique();
+            $table->boolean('perfil');
+            $table->string('senha');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+}
